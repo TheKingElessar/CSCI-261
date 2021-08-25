9aa1ede0a9379fd7a0cfab35996f0742ed92ed24
Nathan Panzer <nathanpanzer@mines.edu>
2021-08-25 10:50:38 -0600

    01

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 // I cannot feel my teeth
 
+#include "iostream"
+using namespace std;
+
 int main() {
+    cout << "Hello please enter SSN thank :)" << endl;
+    cout << "SSN: ";
+    string ssn;
+    cin >> ssn;
+    cout << ssn + " added to the database thank you for your service";
 
     return 0;
 }
