88f9d4611ae3398587e73d10993033b976b3afe2
TheKingElessar <wasabimath@gmail.com>
2021-08-25 21:57:04 -0600

    lab

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
-// I cannot feel my teeth
+/* CSCI 261 Lab 1A: Hello World!
+ *
+ * Author: Nathan Panzer
+ *
+ * literally just printing a few words
+ */
 
-#include "iostream"
+#include <iostream> // For cin, cout, etc.
+
 using namespace std;
 
+// Must have a function named "main", which is the starting point of a C++ program.
 int main() {
+
+    /******** INSERT YOUR CODE BELOW HERE ********/
+
+    cout << "Hello world!" << endl;
+    cout << "How are you?" << endl;
+    cout << "   (I'm fine)." << endl;
+
+    /******** INSERT YOUR CODE ABOVE HERE ********/
 
     return 0;
 }
