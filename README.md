5a4eb0456da9c6e198a0cffb066964fae3d75dc8
TheKingElessar <wasabimath@gmail.com>
2021-08-25 21:58:27 -0600

    Revert "lab"
    This reverts commit 88f9d4611ae3398587e73d10993033b976b3afe2.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# control c control v
+# `// I cannot feel my teeth`
+
+---
+
+This repo is for me to keep track of all of my CSCI-261 labs and assignments. Each one is in a different branch.
--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
-/* CSCI 261 Lab 1A: Hello World!
- *
- * Author: Nathan Panzer
- *
- * literally just printing a few words
- */
+// I cannot feel my teeth
 
-#include <iostream> // For cin, cout, etc.
-
+#include "iostream"
 using namespace std;
 
-// Must have a function named "main", which is the starting point of a C++ program.
 int main() {
-
-    /******** INSERT YOUR CODE BELOW HERE ********/
-
-    cout << "Hello world!" << endl;
-    cout << "How are you?" << endl;
-    cout << "   (I'm fine)." << endl;
-
-    /******** INSERT YOUR CODE ABOVE HERE ********/
 
     return 0;
 }
