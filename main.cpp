f40098ad6bc94e176374c09832625f669ca92ada
TheKingElessar <wasabimath@gmail.com>
2021-08-25 22:02:01 -0600

    A1O

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-// I cannot feel my teeth
-
+/* CSCI 261 Lab 1A: Hello World!
+ *
+ * Author: Nathan Panzer
+ *
+ * copy and pasting some words i guess
+ */
 #include "iostream"
 using namespace std;
 
 int main() {
+
+    cout << "Hello world!" << endl;
+    cout << "How are you?" << endl;
+    cout << "   (I'm fine)." << endl;
 
     return 0;
 }
