8500ebdfc414e9aa1efe6887a5707dd4941d7ab5
TheKingElessar <wasabimath@gmail.com>
2021-08-25 21:35:22 -0600

    updated face

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
-// I cannot feel my teeth
+/* CSCI 261 Assignment 1: Hello World and ASCII Art
+ *
+ * Author: Nathan Panzer
+ * Skip Days Used: 0
+ * Skip Days Remaining: 5
+ * Resources used (Office Hours, Tutoring, Other Students, etc & in what capacity):
+ *
+ *
+ * I made ASCII art I suppose
+ */
 
-#include "iostream"
+#include <iostream>
+
 using namespace std;
 
 int main() {
-    cout << "Hello please enter SSN thank :)" << endl;
-    cout << "SSN: ";
-    string ssn;
-    cin >> ssn;
-    cout << ssn + " added to the database thank you for your service";
+
+    //******** INSERT YOUR CODE BELOW HERE ********
+
+    cout <<
+         "                  /////                    \n"
+         "               /////////////               \n"
+         "            ////////...../////             \n"
+         "          ////..................           \n"
+         "          .....ooo.......ooo.....          \n"
+         "          .....oooo.....oooo.....          \n"
+         "          .......................          \n"
+         "          .......................          \n"
+         "           ....wwwwwwwwwwww.....           \n"
+         "           ;:;....wwwwwwww...;;            \n"
+         "            ;;;;;;;:;;;:;;:;;:             \n"
+         "             ;:;;;;;:;;;;:;:;              \n"
+         "               :;:;;:;;;:;;                \n"
+         "                   :;;;:                   \n"
+         << endl;
+
+    // todo: insert part about facts
+
+    //******** INSERT YOUR CODE ABOVE HERE ********
 
     return 0;
 }
