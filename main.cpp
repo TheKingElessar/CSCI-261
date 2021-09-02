1431d2fbe49039f37c5e155bfcef4e93c5c9cab6
TheKingElessar <wasabimath@gmail.com>
2021-09-02 14:12:11 -0600

    header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+/* CSCI 261 Lab 1B: ASCII Art Intro
+ *
+ * Author: Nathan Panzer
+ *
+ * For this lab, you'll get more practice outputting information to the screen by creating creative pictures.
+ */
+
 #include "iostream"
+
 using namespace std;
 
 int main() {
@@ -7,14 +15,13 @@ int main() {
             "  ***\n"
             " *****\n"
             "*******\n"
-            "  ***" << endl;
-
-    cout << "\n\n";
-
-    cout << "/\\   /\\\n"
+            "  ***\n"
+            "\n\n"
+            "/\\   /\\\n"
             "  o o\n"
             " =   =\n"
-            "  ---";
+            "  ---"
+         << endl;
 
     return 0;
 }
