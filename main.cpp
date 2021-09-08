fd957a5d51b9e26d202de00e1c1b98f093c1a692
Nathan Panzer <nathanpanzer@mines.edu>
2021-09-08 10:48:13 -0600

    initial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 // I cannot feel my teeth
 
+#include <iostream>
+#include <ctime>
+
 using namespace std;
 
 int main()
 {
+
+    cout << "Welcome one and all to a round of Rock, Paper, Scissors! (Enter P, R or S)" << endl;
+    cout << "Player 1: ";
+
+    char player1Choice = '0';
+    cin >> player1Choice;
+    cout << "\nPlayer 1 chooses: " << player1Choice << endl;
+
+    srand(time(nullptr));
+
+    int randomChoice = rand();
+    randomChoice = randomChoice % 3;
+    cout << "Computer chooses: " << randomChoice << endl;
 
     return 0;
 }
