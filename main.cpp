906f8984299300e9997f2242672f258c7702e34d
Nathan Panzer <nathanpanzer@mines.edu>
2021-09-10 10:26:44 -0600

    spook

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 
-    cout << "Welcome one and all to a round of Rock, Paper, Scissors! (Enter P, R or S)" << endl;
+    cout << "Welcome one and all to a round of Rock, Paper, Scissors, Lizard, Spock! (Enter R, P, S, L, or O)" << endl;
     cout << "Player 1: ";
 
     char player1Choice = '0';
@@ -27,6 +27,14 @@ int main()
     {
         player1Word = "Scissors";
     }
+    else if (player1Choice == 'l' || player1Choice == 'L')
+    {
+        player1Word = "Lizard";
+    }
+    else if (player1Choice == 'o' || player1Choice == 'O')
+    {
+        player1Word = "Spock";
+    }
     else
     {
         cout << "That's not a real thing..." << endl;
@@ -38,12 +46,14 @@ int main()
     srand(time(nullptr));
 
     int randomChoice = rand();
-    randomChoice = randomChoice % 3;
+    randomChoice = randomChoice % 5;
 
     string computerWord;
     if (randomChoice == 0) computerWord = "Rock";
     if (randomChoice == 1) computerWord = "Paper";
     if (randomChoice == 2) computerWord = "Scissors";
+    if (randomChoice == 3) computerWord = "Lizard";
+    if (randomChoice == 4) computerWord = "Spock";
 
     cout << "Computer chooses: " << computerWord << "\n" << endl;
 
@@ -51,11 +61,19 @@ int main()
     {
         cout << "Both chose " << player1Word << ".\nIt's a tie!" << endl;
     }
-    else if ((player1Word == "Rock" && computerWord == "Scissors") || (player1Word == "Scissors" && computerWord == "Paper") || (player1Word == "Paper" && computerWord == "Rock"))
+    else if ((player1Word == "Rock" && (computerWord == "Scissors" || computerWord == "Lizard")) ||
+             (player1Word == "Paper" && (computerWord == "Rock" || computerWord == "Spock")) ||
+             (player1Word == "Scissors" && (computerWord == "Paper" || computerWord == "Lizard")) ||
+             (player1Word == "Lizard" && (computerWord == "Spock" || computerWord == "Paper")) ||
+             (player1Word == "Spock" && (computerWord == "Scissors" || computerWord == "Rock")))
     {
         cout << player1Word << " beats " << computerWord << ".\nPlayer wins!" << endl;
     }
-    else if ((computerWord == "Rock" && player1Word == "Scissors") || (computerWord == "Scissors" && player1Word == "Paper") || (computerWord == "Paper" && player1Word == "Rock"))
+    else if ((computerWord == "Rock" && (player1Word == "Scissors" || player1Word == "Lizard")) ||
+             (computerWord == "Paper" && (player1Word == "Rock" || player1Word == "Spock")) ||
+             (computerWord == "Scissors" && (player1Word == "Paper" || player1Word == "Lizard")) ||
+             (computerWord == "Lizard" && (player1Word == "Spock" || player1Word == "Paper")) ||
+             (computerWord == "Spock" && (player1Word == "Scissors" || player1Word == "Rock")))
     {
         cout << computerWord << " beats " << player1Word << ".\nComputer wins!" << endl;
     }
