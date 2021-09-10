9af35b68c76c8abfce3360fb5eb1b1c40bf79ceb
Nathan Panzer <nathanpanzer@mines.edu>
2021-09-10 10:08:44 -0600

    Control FLOW

--- main.cpp
+++ main.cpp
@@ -14,14 +14,38 @@ int main()
     char player1Choice = '0';
     cin >> player1Choice;
 
+    string player1Word;
+    if (player1Choice == 'r' || player1Choice == 'R')
+    {
+        player1Word = "Rock";
+    }
+    else if (player1Choice == 'p' || player1Choice == 'P')
+    {
+        player1Word = "Paper";
+    }
+    else if (player1Choice == 's' || player1Choice == 'S')
+    {
+        player1Word = "Scissors";
+    }
+    else
+    {
+        cout << "That's not a real thing..." << endl;
+        return 1;
+    }
 
-    cout << "\nPlayer 1 chooses: " << player1Choice << endl;
+    cout << "Player 1 chooses: " << player1Word << "\n" << endl;
 
     srand(time(nullptr));
 
     int randomChoice = rand();
     randomChoice = randomChoice % 3;
-    cout << "Computer chooses: " << randomChoice << endl;
+
+    string computerChoice;
+    if (randomChoice == 0) computerChoice = "Rock";
+    if (randomChoice == 1) computerChoice = "Paper";
+    if (randomChoice == 2) computerChoice = "Scissors";
+
+    cout << "Computer chooses: " << computerChoice << endl;
 
     return 0;
 }
