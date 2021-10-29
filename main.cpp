85d2ef1e3bff4b4869d3d9ef83829a2eb6ccb9c6
Nathan Panzer <wasabimath@gmail.com>
2021-10-29 10:50:08 -0600

    k

--- main.cpp
+++ main.cpp
@@ -8,12 +8,23 @@
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int *sortDiceArray(const int dieRolled[]);
+void sortDiceArray(const int dieRolled[], int targetArray[]);
+
+vector<int> removeDuplicates(int targetArray[5]);
 
 bool isSmallStraight(int die1, int die2, int die3, int die4, int die5);
+
+bool isFours(int die1, int die2, int die3, int die4, int die5);
+
+int scoreFours(int die1, int die2, int die3, int die4, int die5);
+
+int scoreSmallStraight(int die1, int die2, int die3, int die4, int die5);
+
+const int SMALL_STRAIGHT_SCORE = 30;
 
 int main()
 {
@@ -24,98 +35,176 @@ int main()
     for (int i = 0; i < 5; i++)
     {
         dieRolled[i] = rand() % 6 + 1;
+        cout << dieRolled[i] << endl;
     }
 
-    bool isRollFours = isSmallStraight(dieRolled[0], dieRolled[1], dieRolled[2], dieRolled[3], dieRolled[4]);
-
+    bool rollIsSmallStraight = isSmallStraight(dieRolled[0], dieRolled[1], dieRolled[2], dieRolled[3], dieRolled[4]);
+    cout << "Roll is small straight: " << rollIsSmallStraight << endl;
+    cout << "Score: " << scoreSmallStraight(dieRolled[0], dieRolled[1], dieRolled[2], dieRolled[3], dieRolled[4]) << endl;
+    bool rollIsFours = isFours(dieRolled[0], dieRolled[1], dieRolled[2], dieRolled[3], dieRolled[4]);
+    cout << "Roll is fours: " << rollIsFours << endl;
+    cout << "Score: " << scoreFours(dieRolled[0], dieRolled[1], dieRolled[2], dieRolled[3], dieRolled[4]) << endl;
     return 0;
 }
 
+/*
+ * Determine if scoring as fours will award points
+ */
 bool isFours(int die1, int die2, int die3, int die4, int die5)
 {
-    /*
     int dieRolled[5] = {die1, die2, die3, die4, die5};
     int fourCount = 0;
-    for (int i = 0; i < 5; i++)
+    for (int i : dieRolled)
     {
-        if (dieRolled[i] == 4)
+        if (i == 4)
         {
             fourCount++;
         }
     }
     return fourCount > 0;
-    */
-
-    return true;
 }
 
-int * sortDiceArray(const int dieRolled[5])
+/*
+ * Returns score of fours
+ */
+int scoreFours(int die1, int die2, int die3, int die4, int die5)
 {
-    int arrayCopy[5];
+    int dieRolled[5] = {die1, die2, die3, die4, die5};
+    int fourCount = 0;
+    for (int i : dieRolled)
+    {
+        if (i == 4)
+        {
+            fourCount++;
+        }
+    }
+    return fourCount * 4;
+}
+
+/*
+ * Removes duplicates from a sorted array of numbers > -1
+ */
+vector<int> removeDuplicates(int targetArray[5])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (targetArray[i] == targetArray[i + 1])
+        {
+            targetArray[i] = -1;
+        }
+    }
+    vector<int> removedDuplicates;
     for (int i = 0; i < 5; i++)
     {
-        arrayCopy[i] = dieRolled[i];
+        if (targetArray[i] != -1)
+        {
+            removedDuplicates.push_back(targetArray[i]);
+        }
+    }
+
+    return removedDuplicates;
+}
+
+/*
+ * Sorts an array of 5 numbers
+ */
+void sortDiceArray(const int dieRolled[5], int targetArray[5])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        targetArray[i] = dieRolled[i];
     }
 
     for (int j = 0; j < 5; j++)
     {
         int smallestIndex = j;
-        int smallestValue = arrayCopy[smallestIndex];
+        int smallestValue = targetArray[smallestIndex];
         for (int i = j; i < 5; i++)
         {
-            if (arrayCopy[i] < smallestValue)
+            if (targetArray[i] < smallestValue)
             {
                 smallestIndex = i;
-                smallestValue = arrayCopy[i];
+                smallestValue = targetArray[i];
             }
         }
 
-        int original = arrayCopy[j];
-        arrayCopy[j] = smallestValue;
-        arrayCopy[smallestIndex] = original;
+        int original = targetArray[j];
+        targetArray[j] = smallestValue;
+        targetArray[smallestIndex] = original;
     }
-
-    for (int i : arrayCopy)
-    {
-        cout << i << endl;
-    }
-
-    return arrayCopy;
 }
 
-int scoreFours(int die1, int die2, int die3, int die4, int die5)
-{
-}
-
+/*
+ * Determine if scoring as small straight will award points
+ */
 bool isSmallStraight(int die1, int die2, int die3, int die4, int die5)
 {
     int dieRolled[5] = {die1, die2, die3, die4, die5};
-    int *sortedRolls[] = sortDiceArray(dieRolled);
-    bool gap = false;
+    int sortedRolls[5];
+    sortDiceArray(dieRolled, sortedRolls);
+    vector<int> deDuped = removeDuplicates(sortedRolls);
 
-    for (int i = 0; i < 5; i++)
+    if (deDuped.size() < 4)
     {
-        if (i != 0)
+        return false;
+    }
+
+    bool firstInvalid = false;
+    bool lastInvalid = false;
+    bool gap = false;
+    for (int i = 0; i < deDuped.size(); i++)
+    {
+        if (i == 0)
         {
-            if (sortedRolls[i] != sortedRolls[i - 1] + 1)
+            if (deDuped.at(i) != deDuped.at(i + 1) - 1)
             {
-                cout << "Index " << i << " invalid" << endl;
+                firstInvalid = true;
+            }
+        }
+        else if (i != deDuped.size() - 1)
+        {
+            if (i == deDuped.size() - 2 && lastInvalid) continue;
+
+            if (deDuped.at(i) != deDuped.at(i + 1) - 1)
+            {
                 gap = true;
                 break;
             }
         }
-        if (i != 5)
+
+
+        if (i == deDuped.size() - 1)
         {
-            if (sortedRolls[i] != sortedRolls[i + 1] - 1)
+            if (deDuped.at(i) != deDuped.at(i - 1) + 1)
             {
-                cout << "Index " << i << " invalid" << endl;
+                lastInvalid = true;
+            }
+        }
+        else if (i != 0)
+        {
+            if (i == 1 && firstInvalid) continue;
+
+            if (deDuped.at(i) != deDuped.at(i - 1) + 1)
+            {
                 gap = true;
                 break;
             }
         }
     }
+    return !gap;
 }
 
+/*
+ * Returns score of small straight
+ */
 int scoreSmallStraight(int die1, int die2, int die3, int die4, int die5)
 {
+    if (isSmallStraight(die1, die2, die3, die4, die5))
+    {
+        return SMALL_STRAIGHT_SCORE;
+    }
+    else
+    {
+        return 0;
+    }
 }
