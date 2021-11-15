f91c0a5a97738a542cf2da6ae60345ebb480ae43
Nathan Panzer <wasabimath@gmail.com>
2021-11-15 10:41:30 -0700

    finished

--- main.cpp
+++ main.cpp
@@ -45,26 +45,67 @@ int main()
         window.draw(targetCircle);
 
         RectangleShape treeTrunk;
-        treeTrunk.setPosition(400, 500);
+        treeTrunk.setPosition(400, 200);
         treeTrunk.setFillColor(Color::Blue);
-        treeTrunk.setSize(Vector2f(25, 100));
+        treeTrunk.setSize(Vector2f(25, 150));
+        window.draw(treeTrunk);
 
-// Draw a text object calledlabel
-        Font myFont;
-        if (!myFont.loadFromFile("data/arial.ttf"))
+        Font arialFont;
+        if (!arialFont.loadFromFile("data/arial.ttf"))
         {
             return -1;
         }
+
+        Text label1;
+        label1.setFont(arialFont);
+        label1.setString("its a FREAKING tree");
+        label1.setPosition(400, 90);
+        label1.setCharacterSize(15);
+        label1.setFillColor(Color::White);
+        window.draw(label1);
+
+        Text label2;
+        label2.setFont(arialFont);
+        label2.setString("gonna start packaging a bitcoin miner in my\ncsci261 assignments to take advantage\nof the nice TAs\n\nsorry");
+        label2.setPosition(400, 550);
+        label2.setCharacterSize(10);
+        label2.setFillColor(Color::White);
+        window.draw(label2);
+
+        for (int i = 0; i < 10; i++)
+        {
+            RectangleShape newBranch;
+            int x = 400;
+            int y = 200;
+            int angle = 45;
+            if (i % 2 != 0)
+            {
+                y += 10 * (i - 1) + 7;
+                x = 425;
+                angle = -45;
+            }
+            else
+            {
+                y += 10 * i;
+            }
+
+            newBranch.setPosition(x, y);
+            newBranch.setFillColor(Color::Green);
+            newBranch.setSize(Vector2f(10, 50));
+            newBranch.setRotation(angle);
+            window.draw(newBranch);
+        }
+// Draw a text object calledlabel
         Text label;
-        label.setFont(myFont);
+        label.setFont(arialFont);
         label.setString("Click the dumb idiot circle to kill it! :)");
-        label.setPosition(100, 520);
+        label.setPosition(80, 460);
         label.setCharacterSize(25);
         label.setFillColor(Color::White);
         window.draw(label);
 
         Text pointsLabel;
-        pointsLabel.setFont(myFont);
+        pointsLabel.setFont(arialFont);
         pointsLabel.setString("Points: " + std::to_string(points));
         pointsLabel.setPosition(100, 100);
         pointsLabel.setCharacterSize(25);
