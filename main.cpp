0bf34ba7ddf05318c3fa1d937ae0e7ab0d75433f
TheKingElessar <wasabimath@gmail.com>
2021-12-06 18:34:38 -0700

    Final
    Took 6 minutes

--- main.cpp
+++ main.cpp
@@ -13,14 +13,17 @@ using namespace std;
 
 int main()
 {
+    cout << "Default Date constructor with explicit Event constructor:" << endl;
     Date date1;
     Event event1(date1, "Event1", "Location1");
     event1.print();
 
+    cout << endl << "Explicit Date constructor with explicit Event constructor:" << endl;
     Date date2(2012, 8, 28);
     Event event2(date2, "Event2", "Location2");
     event2.print();
 
+    cout << endl << "Default Event constructor:" << endl;
     Event event3;
     event3.print();
 
